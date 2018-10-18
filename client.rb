51ea132ceae8d971b3eae65e9fa1a3f7982448c2
Arturo Cortes <31288940+Arson123@users.noreply.github.com>
2018-10-18 14:01:56 -0500

    added new class Client

---
+++ client.rb
@@ -0,0 +1,7 @@
+#Client object
+class Client
+	timeOnPaydesk = 0
+	clientArray = Array.new
+	clientArray = ['A','B','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+	 
+end
