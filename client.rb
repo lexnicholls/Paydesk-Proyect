29d01a2ac5b80e62dd587ff83d39e2fdcf6d1b17
Arturo Cortes <31288940+Arson123@users.noreply.github.com>
2018-10-18 14:01:56 -0500

    Updated class Client

---
+++ client.rb
@@ -0,0 +1,23 @@
+#Client object
+class Client
+
+
+   def initialize()
+    @clientArray = Array.new
+	@clientArray = ['A','B','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+   end
+
+   def fileCheckout()
+    
+   end
+
+   def setBuyingTime()
+    timeOnPaydesk=rand(4..25)  #time given on minutes    
+    return timeOnPaydesk
+   end
+
+   def returnClients()
+    randomClient = rand(0..24)
+    return @clientArray[randomClient]
+   end	 
+end
