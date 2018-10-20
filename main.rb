56c9b4669573269221d46b3bfdd1075bd2fd47e7
Arturo Cortes <nelsonarturocortes@gmail.com>
2018-10-19 20:51:17 -0500

    Add the menu and the options with your respective methods

--- main.rb
+++ main.rb
@@ -1,8 +1,21 @@
 #Main class
 
-class Main
+class Main 
 	iterator = 0.to_i
 	queueArray = Array.new
 	simulation_Time = 0 #Minute input
 	iteration_Time = 0 #Seconds input
+end
+
+class Menu (option)
+	case(option)
+		when 1:
+			uniqQueue()
+			break
+		when 2:
+			multipleQueue()
+			break
+		else
+			puts "the program don´t reconoce the name"
+	end
 end
