957d071214d1b97ee6992ef8a6f7075cc4a93231
Arturo Cortes <nelsonarturocortes@gmail.com>
2018-10-19 22:08:27 -0500

    Update queue class

--- queue.rb
+++ queue.rb
@@ -1,11 +1,22 @@
 #Queue object
 class Queue
+
 	def initialize()
+		@queue_Client = Array.new
+		@total_Time = 0
+
 	end
-	queue_Client = Array.new
-	@total_Time = 0
 
 	def clientsArrival()
 		arrivingClients = rand(0..5)
 	end
+
+	def getClient()
+			@queue_Client.push(Client.new())
+	end
+
+	def printClient()
+
+  end
+
 end
