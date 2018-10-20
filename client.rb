d6373a275e174f1ade5fa60d868823442b1b31fb
Alexander Nicholls <nicholls.aom@gmail.com>
2018-10-19 23:40:49 -0500

    Added new method on client class

---
+++ client.rb
@@ -0,0 +1,26 @@
+#Client object
+class Client
+
+   def initialize
+     @clientArray = Array.new()
+	   @clientArray = ['A','B','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+     @clientObject = Array.new()
+     @clientObject = [generateClient, rand(4..25)]
+   end
+
+   ##def queueCheckoutMethod
+
+   def generateClient
+    randomClient = rand(0..24)
+    clientName = @clientArray[randomClient]
+	return clientName
+   end
+
+   def returnClient
+     return @clientObject[0]
+   end
+
+   def returnTime
+     return @clientObject[1]
+   end
+end
