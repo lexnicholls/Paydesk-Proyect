178facb08419cf185bcfa6b4483ac71facaad64f
Alexander Nicholls <nicholls.aom@gmail.com>
2018-10-20 00:12:09 -0500

    Updated menu on Main class

--- main.rb
+++ main.rb
@@ -1,33 +1,40 @@
-#Main class
+#M#Main class
 
 class Main ()
 
 	def initialize()
 		iterator = 0.to_i
 		queueArray = Array.new
+		puts "Input simulation time on minutes."
 		simulation_Time = gets#Minute input
+		puts "Input iteration time on seconds"
 		iteration_Time = gets #Seconds input
+		puts "Input queue system:"
+		puts "1. Unique queue."
+		puts "2. Multiple queue."
+		Menu(gets)
 	end
 
 	def Menu (option)
-	  case(option)
-		when 1:
+	  case(option.to_i)
+		when 1
 			uniqQueue()
-			break
-		when 2:
+		when 2
 			multipleQueue()
-			break
 		else
 			puts "the program don´t reconoce the name"
+			initialize()
      end
    end
 
    def	uniqQueue()
-
+		 puts "uniqQueue"
    end
 
    def multipleQueue()
+		 puts "multipleQueue"
    end
 
-
 end
+
+menu = Main.new()
