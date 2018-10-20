4606747caa2345f0ff87359b721ed4e9417abbf5
Arturo Cortes <nelsonarturocortes@gmail.com>
2018-10-20 12:43:35 -0500

    Update the main class in the multipleQueue method

--- main.rb
+++ main.rb
@@ -5,43 +5,43 @@ load 'queue.rb'
 
 class Main ()
 
-	def initialize()
-	    @checkout = Checkout.new()
-		@queue = Queue.new()
-		iterator = 0.to_i
-		@queueArray = []
-		@checkoutArray = Array.new()
-		puts "Input simulation time on minutes."
-		simulation_Time = gets#Minute input
-		puts "Input iteration time on seconds"
-		iteration_Time = gets #Seconds input
-		puts "Input queue system:"
-		puts "1. Unique queue."
-		puts "2. Multiple queue."
-		Menu(gets)
-	end
+    def initialize()
+        @checkout = Checkout.new()
+        @queue = Queue.new()
+        iterator = 0.to_i
+        @queueArray = []
+        @checkoutArray = Array.new()
+        puts "Input simulation time on minutes."
+        simulation_Time = gets#Minute input
+        puts "Input iteration time on seconds"
+        iteration_Time = gets #Seconds input
+        puts "Input queue system:"
+        puts "1. Unique queue."
+        puts "2. Multiple queue."
+        Menu(gets)
+    end
 
-	def Menu (option)
-	  case(option.to_i)
-		when 1
-			uniqQueue()
-		when 2
-			multipleQueue()
-		else
-			puts "The simulator didn't recognize the input."
-			initialize()
+    def Menu (option)
+      case(option.to_i)
+        when 1
+            uniqQueue()
+        when 2
+            multipleQueue()
+        else
+            puts "The simulator didn't recognize the input."
+            initialize()
      end
    end
 
-   def	uniqQueue()
-		 puts "uniqQueue"
+   def    uniqQueue()
+         puts "uniqQueue"
    end
 
    def multipleQueue()
-		 puts "multipleQueue"
-		 @checkoutArray.push(@checkout.checkoutName())
-		 puts @checkoutArray
-		 @queueArray.push(@queue.clientsArrival())
+         puts "multipleQueue"
+         @checkoutArray.push(@checkout.checkoutName())
+         puts @checkoutArray
+         @queueArray.push(@queue.clientsArrival())
    end
 
 end
--- queue.rb
+++ queue.rb
@@ -3,26 +3,26 @@ load 'client.rb'
 
 class Queue
 
-	def initialize()
-		@queue_Client = Array.new
-		@total_Time = 0
-	end
+    def initialize()
+        @queue_Client = Array.new
+        @total_Time = 0
+    end
 
-	def clientsArrival()
-		return getClient(rand(0..5))
-	end
+    def clientsArrival()
+        return getClient(rand(0..5))
+    end
 
-	def getClient(random)
-		@i=0
-		while @i<random
-		    client = Client.new()
-			@queue_Client.push("|" + client.returnClient + "|")
-			@i=@i+1
-		end
-		printClient()
-	end
+    def getClient(random)
+        @i=0
+        while @i<random
+            client = Client.new()
+            @queue_Client.push("|" + client.returnClient + "|")
+            @i=@i+1
+        end
+        printClient()
+    end
 
-	def printClient()
-		puts @queue_Client
-	end
+    def printClient()
+        puts @queue_Client
+    end
 end
